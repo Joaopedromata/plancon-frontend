6f77050bf9cbcec521d56a804eeb6e29dec7f7bb
Joaopedromata <joaopmata182@gmail.com>
2020-10-20 16:55:06 -0300

    trying to sum quantity

--- src/pages/StorageLocation/index.js
+++ src/pages/StorageLocation/index.js
@@ -12,8 +12,35 @@ import {
     BodyTable
 } from './styles'
 import Header from '../../components/Header'
+import { useEffect } from 'react'
+import api from '../../service/api'
 
 const Storage = ({ location }) => {
+
+    const arr = []
+    useEffect(() => {
+        api.get('/storages/inputs/quantity/sum/1/1').then(res => {
+            const teste = res.data.plancons
+            
+            teste.map(oi => {
+                const testando = oi.outputs
+
+
+
+                testando.map(opa => {
+                    return arr.push({quantity: opa.quantity})
+                })
+                
+            } )
+
+            
+            
+        })
+
+        console.log(arr)
+    }, [])
+
+
     return (
         <Container>
             <Header title="Esses são os materiais disponíveis na obra VNO" back="/estoque" infosBack={location.state} />
